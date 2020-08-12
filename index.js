18364fdbb9380aa326f560ad5566bf7804f789fc
Eric Park <epark0995@gmail.com>
2020-08-11 17:39:48 -0700

    got title prompt working

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-//notes
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./generateMarkdown.js')
+
 // array of questions for user
 const questions = [
+    inquirer
+      .prompt([
+          {
+          type: 'input',
+          name: 'title',
+          message: 'What is your title'
+
+          }])
 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
 
-}
 
-// function call to initialize program
-init();
+//function to write the file to readme
+// fs.writeFile('exREADME.md', 'Hi', (err) => {
+// 	if (err) {console.log(err)}
+// 	console.log('File Create!')})
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
