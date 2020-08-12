acfa8a58587d5c55a907ca54ed1f01f8f731bd62
Eric Park <epark0995@gmail.com>
2020-08-11 18:28:31 -0700

    added all questions and generate markdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,38 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+  #${answers.title}
+  ![badge](https://img.shields.io/badge/license-${answers.license}-blue.svg)
 
+  ##Description
+    ${answers.description} 
+
+  ##Table of Contents 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+  
+  ##Installation
+  ${answers.install}
+
+  ##Usage
+  ${answers.usage}
+
+  ##Contributing
+  ${answers.contribution}
+
+  ##Tests
+
+  ##Questions
+  If you have any questions you can find me on Github: [${answers.username}](https://github.com/${answers.username})
+  or Email me at: ${answers.email}
 `;
+
+  
 }
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -11,8 +11,51 @@ const questions = [
           name: 'title',
           message: 'What is your title'
 
-          }])
+          },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Give a description for your README'  
+        },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How to install your application'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'List contribution guidelines'
+    },
 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license?',
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?' 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+])
+    .then(answers => {
+        console.log(answers)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 ];
 
 // // function to write README file
